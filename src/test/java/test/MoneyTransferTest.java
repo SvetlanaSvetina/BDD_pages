0ad7b4a6b3d2973602b279eec0140e664202033b
SvetlanaVasilek <vasilek378@gmail.com>
2022-10-29 23:25:46 +0300

    initial commit

--- src/test/java/test/MoneyTransferTest.java
+++ src/test/java/test/MoneyTransferTest.java
@@ -6,6 +6,9 @@ import org.junit.jupiter.api.Test;
 import page.LoginPage;
 
 import static com.codeborne.selenide.Selenide.open;
+import static data.DataHelper.getFirstCardInfo;
+import static data.DataHelper.getSecondCardInfo;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class MoneyTransferTest {
     @Test
@@ -17,6 +20,40 @@ class MoneyTransferTest {
         var verificationPage = loginPage.validLogin(authInfo);
         var verificationCode = DataHelper.getVerificationCodeFor(authInfo);
         var dashboardPage = verificationPage.validVerify(verificationCode);
+        var firstCardInfo = getFirstCardInfo();
+        var secondCardInfo = getSecondCardInfo();
+        int amount = 5000;
+        var expectedBalanceFirstCard = dashboardPage.getCardBalance(firstCardInfo) - amount;
+        var expectedBalanceSecondCard = dashboardPage.getCardBalance(secondCardInfo) + amount;
+        var transferPage = dashboardPage.selectCardToTransfer(secondCardInfo);
+        dashboardPage = transferPage.makeTransfer(Integer.parseInt(String.valueOf(amount)), firstCardInfo);
+        var actualBalanceFirstCard = dashboardPage.getCardBalance(firstCardInfo);
+        var actualBalanceSecondCard = dashboardPage.getCardBalance(secondCardInfo);
+        assertEquals(expectedBalanceFirstCard, actualBalanceFirstCard);
+        assertEquals(expectedBalanceSecondCard, actualBalanceSecondCard);
+    }
 
+    @Test
+    void TransferFrom2To1() {
+        Configuration.holdBrowserOpen = true;
+
+        var loginPage = open("http://localhost:9999", LoginPage.class);
+        var authInfo = DataHelper.getAuthInfo();
+        var verificationPage = loginPage.validLogin(authInfo);
+        var verificationCode = DataHelper.getVerificationCodeFor(authInfo);
+        var dashboardPage = verificationPage.validVerify(verificationCode);
+        var firstCardInfo = getFirstCardInfo();
+        var secondCardInfo = getSecondCardInfo();
+        int amount = 3000;
+        var expectedBalanceFirstCard = dashboardPage.getCardBalance(firstCardInfo) + amount;
+        var expectedBalanceSecondCard = dashboardPage.getCardBalance(secondCardInfo) - amount;
+        var transferPage = dashboardPage.selectCardToTransfer(firstCardInfo);
+        dashboardPage = transferPage.makeTransfer(Integer.parseInt(String.valueOf(amount)), secondCardInfo);
+        var actualBalanceFirstCard = dashboardPage.getCardBalance(firstCardInfo);
+        var actualBalanceSecondCard = dashboardPage.getCardBalance(secondCardInfo);
+        assertEquals(expectedBalanceFirstCard, actualBalanceFirstCard);
+        assertEquals(expectedBalanceSecondCard, actualBalanceSecondCard);
     }
 }
+
+
